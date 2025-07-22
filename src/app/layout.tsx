6002f27dc0f2a55e494d670e656a18e05474288c
wwphuztr <phunguyen.tester@gmail.com>
2025-07-22 14:57:59 +0700

    Remove unused import for Script component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
+// import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
