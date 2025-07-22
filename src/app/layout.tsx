b46e179f51ce4042168a0a6b2761598e692348df
wwphuztr <phunguyen.tester@gmail.com>
2025-07-22 15:05:09 +0700

    Refactor script tag in layout component for improved readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// import Script from "next/script";
+import Head from "next/head";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,16 +26,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-         <script
+        <script
+          defer
           src="https://api-ads.attoaioz.cyou/api/v1/platform/download_header_script_file.js?orgId=ce6faa21-416e-4f77-915b-1e759d91b847&platformId=50fd8f60-2b07-446e-a20b-9ac11feeefb1"
-          crossOrigin="anonymous"
-        />
+          crossOrigin="anonymous">
+        </script>
       </head>
-
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      
+        {/* <Script
+          src="https://api-ads.attoaioz.cyou/api/v1/platform/download_header_script_file.js?orgId=ce6faa21-416e-4f77-915b-1e759d91b847&platformId=50fd8f60-2b07-446e-a20b-9ac11feeefb1"
+          strategy="beforeInteractive"
+        /> */}
         {children}
       </body>
     </html>
