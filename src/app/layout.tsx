de0c1ccfdbdd95d04465f9d3d339982a72a40f59
wwphuztr <phunguyen.tester@gmail.com>
2025-07-23 14:43:23 +0700

    Update AIOZ ads script source URL in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <head>
         <script
           defer
-          src="https://api-ads.attoaioz.cyou/api/v1/platform/download_header_script_file.js?orgId=ce6faa21-416e-4f77-915b-1e759d91b847&platformId=50fd8f60-2b07-446e-a20b-9ac11feeefb1"
+          src="https://api-ads.attoaioz.cyou/api/v1/platform/download_header_script_file.js?orgId=ce6faa21-416e-4f77-915b-1e759d91b847&platformId=55080037-615f-4021-98b4-7cfe4d120413"
           crossOrigin="anonymous">
         </script>
       </head>
